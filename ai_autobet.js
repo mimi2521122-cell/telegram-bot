834ec4132e6177fbaead4030ed14f36a68e2be91
mimi2521122-cell <mimi2521122@gmail.com>
2026-05-07 11:01:10 +0630

    Update ai_autobet.js

--- ai_autobet.js
+++ ai_autobet.js
@@ -7132,7 +7132,7 @@ async function showUserStats(ctx, userId) {
 
 // ⚙️ Config
 const BASE_URL = PLATFORMS["CKLOTTERY"].baseUrl;
-const BOT_TOKEN = "8707027344:AAGLf5jUW8MGlepK4MKfdTWWSYbwve728q8";
+const BOT_TOKEN = "8533662019:AAFbaosFVvTL31GbV_6UmxKIV5M3ABeTyVc";
 const ADMIN_ID = 7308292609;
 const IGNORE_SSL = true;
 const WIN_LOSE_CHECK_INTERVAL = 2;
